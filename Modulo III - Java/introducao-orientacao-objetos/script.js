7e9363b8aa2c32774b7c6de6e6c148b5f1aace1b
Thiago <thiagonascimento0013@gmail.com>
2022-04-29 22:04:10 -0300

    Aulas Java

---
+++ Modulo III - Java/introducao-orientacao-objetos/script.js
@@ -0,0 +1,86 @@
+/*
+Atividade: Conta Bancária
+Nesta atividade, vamos testar os conceitos de Orientação a Objetos simulando a criação de diversos tipos de contas bancárias e operações disponíveis em cada uma.
+
+Crie a classe ContaBancaria, que possui os parâmetros agencia, numero, tipo e saldo;
+Dentro de ContaBancaria, construa o getter e o setter de saldo;
+Dentro de ContaBancaria, crie os métodos sacar e depositar;
+Crie uma classe-filha chamada ContaCorrente que herda todos esses parâmetros e ainda possua o parâmetro cartaoCredito;
+Ainda em ContaCorrente, construa o getter e o setter de cartaoCredito;
+Ainda em ContaCorrente, faça com que o tipo seja 'conta corrente' por padrão;
+Crie uma classe-filha chamada ContaPoupanca que herda todos os parâmetros de ContaBancaria;
+Crie uma classe-filha chamada ContaUniversitaria que herda todos os parâmetros de ContaBancaria;
+Faça com que o método saque de ContaUniversitaria apenas seja capaz de sacar valores menores que 500 reais.
+*/
+
+class ContaBancaria {
+    constructor(agencia, numero, tipo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    set saldo(valor) {
+        this._saldo = valor;
+    }
+
+    sacar (valor) {
+        if (valor > this._saldo){
+            return ('Operação negada');
+        }
+        this._saldo = this._saldo - valor;
+
+        return this._saldo;
+    }
+
+    depositar (valor) {
+        if (valor <= 0) {
+            return ('O valor do depósito deve ser maior que zero');
+        }
+        this._saldo = this._saldo + valor;
+
+        return this._saldo;
+    }
+}
+
+class ContaCorrente extends ContaBancaria {
+    constructor(agencia, numero, cartaoCredito) {
+        super(agencia, numero);
+        this.tipo = 'corrente';
+        this._cartaoCredito = cartaoCredito;
+    }
+
+    get cartaoCredito() {
+        return this._cartaoCredito;
+    }
+    
+    set cartaoCredito(valor) {
+        this._cartaoCredito = valor;
+    }
+}
+
+class ContaPoupanca extends ContaBancaria {
+    constructor(agencia, numero) {
+        super(agencia, numero);
+        this.tipo = 'poupança';
+    }
+}
+
+class ContaUniversitaria extends ContaBancaria {
+    constructor(agencia, numero) {
+        super(agencia, numero);
+        this.tipo = 'universitaria';
+    }
+
+    saque (valor){
+        if (valor > 500){
+            return ('Operação negada');
+        }
+        this._saldo = saldo - valor;
+    }
+}
